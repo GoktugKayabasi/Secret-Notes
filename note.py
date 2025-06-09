30f3076fb50c192c0198e35d80ea12e855cfb05f
Göktuğ KAYABAŞI <goktugkybsi@gmail.com>
2025-06-09 21:33:35 +0300

    UI added

--- note.py
+++ note.py
@@ -1,5 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import base64
+
+def encode(key,clear):
+    enc=[]
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key,enc):
+    dec=[]
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256+ ord (enc [i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+
+
+
+
+
+
+
+
+
+
 
 def save_and_encrypt_notes():
     title=title_entry.get()
@@ -8,17 +38,20 @@ def save_and_encrypt_notes():
 
     if len(title) == 0 or len(message) == 0 or len(master_secter) == 0:
         messagebox.showinfo(title="Error!!!",message="Pleas enter all info.")
+    else:
+        #encryption
+        message_encrypted = encode(master_secter,message)
 
-
-
-
-
-
-
-
-
-
-
+        try:
+            with open("mysecret.txt","a") as data_file:
+                data_file.write(f"\n{title}\n{message_encrypted}")
+        except FileNotFoundError:
+            with open ("mysecret.txt","w") as data_file:
+                data_file.write(f"\n{title}\n{message_encrypted}")
+        finally:
+            title_entry.delete(0,END)
+            master_secret_input.delete(0, END)
+            input_text.delete("1.0",END)
 
 
 
